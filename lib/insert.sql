ea28bba3a910bb7ddeb8a1b53e33e564d926ff77
Cody Frank <cody.frank30@gmail.com>
2019-06-23 21:03:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -12,7 +12,7 @@ VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
-;INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
