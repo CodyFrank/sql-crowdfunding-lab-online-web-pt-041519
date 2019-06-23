4a00c97cb08f63298eca8d4b074cf4c3c0db518a
Cody Frank <cody.frank30@gmail.com>
2019-06-23 21:05:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -27,9 +27,6 @@ VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("title_1", "test", 10, 06-22-19, 06-23-19);
-
 INSERT INTO users (name, age)
 values ("name", 1);
 
