839fbce2e2a28888bbddf0a50e46f48c955e8570
Cody Frank <cody.frank30@gmail.com>
2019-06-23 20:38:52 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,92 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("title_1", "test", 10, 06-22-19, 06-23-19)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
+
+INSERT INTO users (name, age)
+values ("name", 1)
